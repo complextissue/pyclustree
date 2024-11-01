58a247a20c7c511e62a377a34ebae4933e14cc30
harryhaller001 <40577985+harryhaller001@users.noreply.github.com>
2024-11-01 17:48:26 +0100

    updating workflow action versions, adding latest python version to actions, adding repo link to readme, adding error handling test functions to pytest, adding latest python version to pytest

--- pyclustree/__init__.py
+++ pyclustree/__init__.py
@@ -2,8 +2,6 @@ from importlib.metadata import version
 
 from ._clustree import clustree
 
-pyclustree = clustree
-
-__all__ = ["clustree", "pyclustree"]
+__all__ = ["clustree"]
 
 __version__ = version("pyclustree")
